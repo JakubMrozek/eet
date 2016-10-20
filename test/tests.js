6273b32601ee8493a14929e17b446159a68ddfc2
JakubMrozek <jakub.mrozek@gmail.com>
2016-10-20 02:22:57 +0200

    send soap request

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,13 @@
+import fs from 'fs'
 import test from 'ava'
 import eet from '..'
-import {TEST_PKP, TEST_PRIVATE_KEY} from './data'
 
+const PRIVATE_KEY = fs.readFileSync('./keys/private.pem')
+const CERTIFICATE = fs.readFileSync('./keys/certificate.pem')
+const TEST_PKP = 'JvCv0lXfT74zuviJaHeO91guUfum1MKhq0NNPxW0YlBGvIIt+I4QxEC3QP6BRwEkIS14n2WN+9oQ8nhQPYwZX7L4W9Ie7CYv1ojcl/YiF4560EdB3IpRNRj3UjQlwSZ5ucSM9vWqp0UTbhJDSUk5/WjC/CEiSYv7OQIqa0NJ0f0+ldzGveLRSF34eu2iqAhs/yfDnENlnMDPVB5ko/zQO0vcC93k5DEWEoytTIAsKd6jKSO7eama8Qe+d0wq9vBzudkfLgCe2C1iERJuyHknhjo9KOx10h5wk99QqVGX8tthpAmryDcX2N0ZGkzJHuzzebnYsxXFYI2tKOJLiLLoLQ=='
+
+
+/*
 test('generate PKP', t => {
   const result = eet.generatePKP(
     TEST_PRIVATE_KEY,
@@ -14,6 +20,7 @@ test('generate PKP', t => {
   )
   t.is(result, TEST_PKP)
 })
+*/
 
 test('generate BKP', t => {
   t.is(eet.generateBKP(TEST_PKP), '3F9119C1-FBF34535-D30B60F8-9859E4A6-C8C8AAFA')
@@ -22,4 +29,8 @@ test('generate BKP', t => {
 test('date to ISO', t => {
   const date = new Date('2016-08-05T00:30:12+02:00')
   t.is(eet.dateToISO(date), '2016-08-04T22:30:12Z')
+})
+
+test('send', async t => {
+  const result = await eet.send(PRIVATE_KEY, CERTIFICATE)
 })
---
+++ ws-security.js
@@ -0,0 +1,71 @@
+const ursa = require('ursa')
+const uuid = require('node-uuid')
+const {SignedXml} = require('xml-crypto')
+
+const SIGNATURE_ALGORITHM = 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'
+const TRANSFORM_ALGORITHMS = [
+  'http://www.w3.org/2000/09/xmldsig#enveloped-signature',
+  'http://www.w3.org/2001/10/xml-exc-c14n#'
+]
+const DIGEST_ALGORITHM = 'http://www.w3.org/2001/04/xmlenc#sha256'
+
+function insertStr (src, dst, pos) {
+  return [dst.slice(0, pos), src, dst.slice(pos)].join('')
+}
+
+function generateId () {
+  return 'x509-' + uuid.v4().replace(/-/gm, '')
+}
+
+function removeCertHeaderAndFooter (publicP12PEM) {
+  return publicP12PEM.toString()
+  .replace('-----BEGIN CERTIFICATE-----', '')
+  .replace('-----END CERTIFICATE-----', '')
+  .replace(/(\r\n|\n|\r)/gm, '')
+}
+
+function getTokenXml (id) {
+  return (
+    `<wsse:SecurityTokenReference>
+      <wsse:Reference URI="#${id}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/>
+    </wsse:SecurityTokenReference>`
+  )
+}
+
+function getSecurityXml (id, binaryToken) {
+  return (
+    `<wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+               xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+               soap:mustUnderstand="1">
+    <wsse:BinarySecurityToken
+                EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary"
+                ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"
+                wsu:Id="${id}">${binaryToken}</wsse:BinarySecurityToken>
+    </wsse:Security>`
+  )
+}
+
+class WSSecurityCert {
+
+  constructor (privatePEM, publicP12PEM, password, encoding) {
+    this.privateKey = ursa.createPrivateKey(privatePEM, password, encoding)
+    this.publicP12PEM = removeCertHeaderAndFooter(publicP12PEM)
+    this.x509Id = generateId()
+
+    this.signer = new SignedXml()
+    this.signer.signingKey = this.privateKey.toPrivatePem()
+    this.signer.signatureAlgorithm = SIGNATURE_ALGORITHM
+    this.signer.addReference("//*[local-name(.)='Body']", TRANSFORM_ALGORITHMS, DIGEST_ALGORITHM)
+    this.signer.keyInfoProvider = {}
+    this.signer.keyInfoProvider.getKeyInfo = () => getTokenXml(this.x509Id)
+  }
+
+  postProcess (xml) {
+    const secHeader = getSecurityXml(this.x509Id, this.publicP12PEM)
+    const xmlWithSec = insertStr(secHeader, xml, xml.indexOf('</soap:Header>'))
+    this.signer.computeSignature(xmlWithSec)
+    return insertStr(this.signer.getSignatureXml(), xmlWithSec, xmlWithSec.indexOf('</wsse:Security>'))
+  }
+}
+
+module.exports = WSSecurityCert
